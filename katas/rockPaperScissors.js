0aad06fb88988c016486623d40d284d41be4e2fa
marwanfatalla <117901909+marwanfatalla@users.noreply.github.com>
2022-11-18 11:36:46 +0100

    -Reformated rockPaperScissors

--- katas/rockPaperScissors.js
+++ katas/rockPaperScissors.js
@@ -1,13 +1,20 @@
-function rockPaperScissors(){
+function getPlayerChoice(name){
     const prompt = require("prompt-sync")({ sigint: true });
-    const p1Choice = prompt("Player 1 choice, type 0 for Rock, 1 for Paper, 2 for Scissors")
+    const pChoice = prompt(`${name} choice, type 0 for Rock, 1 for Paper, 2 for Scissors`)
+    return pChoice
+}
+
+function rockPaperScissors(){
+    
+    const p1Choice = getPlayerChoice("Player 1")
     if(p1Choice < 0 || p1Choice > 2){
         return "Wrong value"
     }
-    const p2Choice = prompt("Player 2 choice, type 0 for Rock, 1 for Paper, 2 for Scissors")
-    if(p1Choice < 0 || p1Choice > 2){
+    const p2Choice = getPlayerChoice("Player 2")
+    if(p2Choice < 0 || p2Choice > 2){
         return "Wrong value"
     }
+    
 
     const ROCK = 0
     const PAPER = 1
